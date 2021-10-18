e368733163a3f50fd1aae671a773e91166827bf0
arif_izzeti@outlook.com <arif_izzeti@outlook.com>
2021-10-18 17:54:51 +0300

    Add repository service and check entity framework correctly create db in sql server

---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,82 @@
+﻿using Accounting.DataLayer.Context;
+using Accounting.DataLayer.Repositories;
+using Accounting.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        AccountingContext db = new AccountingContext();
+        public List<Customer> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+
+        public bool InsertCustomer(Customer customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool DeleteCustomer(Customer customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            try
+            {
+                var customer = GetCustomerById(customerId);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
